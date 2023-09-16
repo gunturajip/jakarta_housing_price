21afed1abfae77ac778623581a93c6e2ced42968
darren7753 <matthewdarren7753@gmail.com>
2023-09-16 20:02:15 +0700

    Added logging of webpage source

--- 01_data_scraping.py
+++ 01_data_scraping.py
@@ -119,6 +119,7 @@ for page in range(1, 101):
     # Search for the property elements
     property_elements = driver.find_elements(By.XPATH, "//div[contains(@class, 'card-featured__content-wrapper')]")
     print(property_elements)
+    print(driver.page_source)
     
     # Iterate through Each Property Element
     index = 0
